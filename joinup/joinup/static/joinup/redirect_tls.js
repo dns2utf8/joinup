e12007a509eb639f6790f8c574f65d63c0c2a97c
Stefan Schindler <dns2utf8@estada.ch>
2017-12-03 18:04:55 +0100

    ...

--- joinup/joinup/static/joinup/redirect_tls.js
+++ joinup/joinup/static/joinup/redirect_tls.js
@@ -1,19 +1,7 @@
 (function() {
-var tags = document.querySelectorAll('.grid > *');
-for (var i = 0; i < tags.length; ++i) {
-    var tag = tags[i];
-    tag.style.background = '#'+random_color_part(0xa0, 0xf0)
-                                +random_color_part(0xa0, 0xf0)
-                                +random_color_part(0xa0, 0xf0);
+
+if (location.protocol === 'http:' && location.hostname !== 'localhost') {
+    location.protocol = 'https:';
 }
 
-/// Output two signs as a Hex String
-function random_color_part(min, max) {
-    var r = Math.random() * (max - min) + min;
-    r = (r |0).toString(16);
-    if (r.length === 1) {
-        r = '0'+r;
-    }
-    return r;
-}
 })();
