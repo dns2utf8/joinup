362706e4f18f5ab6a36284bd2f8484efe962e4f6
Stefan Schindler <dns2utf8@estada.ch>
2017-12-04 23:38:34 +0100

    Add ToS

--- joinup/pages/urls.py
+++ joinup/pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^imprint$', views.make_page('Imprint'), name='page_imprint'),
     url(r'^about', views.make_page('About'), name='page_about'),
+    url(r'^terms', views.make_page('Terms of service'), name='page_terms_of_service'),
 ]
